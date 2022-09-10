11ab60d0dcb81a948d7cdf661c9c7d389d992f44
wijdench <chebbi.wijden@gmail.com>
2022-09-10 08:26:05 -0400

    Update ApiGetPokemonImageLink.ts

--- server/service/ApiGetPokemonImageLink.ts
+++ server/service/ApiGetPokemonImageLink.ts
@@ -5,14 +5,8 @@ export default class ApiGetPokemonImageLink {
         return `${baseLink}${fileName}.png`
     }
 
-    private _getFileName({ id }: { id: string }): string{
-        switch (true) {
-            case (id.length === 1) :
-                return `00${id}`
-            case (id.length === 2) :
-                return `0${id}`
-            default:
-                return id
-        }
+    private _getFileName({ id }: { id: string }): string {
+        const fileNameLength = 3
+        return id.padStart(fileNameLength, '0')
     }
 }
